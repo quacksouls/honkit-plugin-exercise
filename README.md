bd968e2fc16d69107c0a62394c8640203e55f6ac
Duck McSouls <quacksouls@gmail.com>
2023-05-22 22:24:33 +1000

    Format and number each exercise, allows cross-referencing

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# honkit-plugin-exercise
-Formatting exercises/problems in HonKit
+# HonKit plugin: exercise
+
+Formats and numbers each exercise. Numbering is automatic and starts at 1. An
+exercise can be labelled to allow for cross-referencing.
+
+## Installation
+
+Install via npm:
+
+```sh
+npm install honkit-plugin-exercise --save-dev
+```
+
+Add the plugin to your `book.json`:
+
+```js
+{
+    plugins: [ "exercise" ]
+}
+```
+
+## Usage
+
+An exercise is added by the block:
+
+```
+{% exercise %}
+Insert your exercise text here.
+{% endexercise %}
+```
+
+To help cross-reference an exercise, you can add a label. The label text should
+not have any white spaces.
+
+```
+{% exercise label="my_label" %}
+Insert your exercise text here.
+{% endexercise %}
+```
+
+For example, the following exercise blocks
+
+```
+{% exercise %}
+What Is the airspeed velocity of an unladen swallow?
+{% endexercise %}
+
+{% exercise label="a_label" %}
+What do you mean? An African or a European swallow?
+{% endexercise %}
+```
+
+would be rendered as
+
+![Unladen swallow](example.png "Unladen swallow")
+
+Cross-reference an exercise like how you cross-reference a section in a Markdown
+file. In the above example, the exercise having the label `"a_label"` can be
+cross-referenced as:
+
+```
+[this exercise](/path/to/file.md#a_label)
+```
--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 module.exports = {
-    // Extend website resources and HTML.
-    // website: {
-    //     assets: "./assets",
-    //     js: [
-    //         "plugin.js"
-    //     ],
-    //     css: [
-    //         "plugin.css"
-    //     ]
-    // },
     hooks: {
-        // Parse the raw Markdown content.
-        "page:before": (page) => {
-            page.content = page.content.replace("!EXERCISE", "<strong>Exercise.</strong>");
+        /**
+         * Number each exercise.  This is run before HonKit outputs HTML and
+         * indexes the page.
+         */
+        "page": (page) => {
+            const regex = /<strong>Exercise.<\/strong>/g;
+            let match = page.content.match(regex);
+            let i = 0;
+
+            while (!!match) {
+                i++;
+                const replace = `<strong>Exercise ${i}.</strong>`;
+                page.content = page.content.replace(match[0], replace);
+                match = page.content.match(regex);
+            }
+
             return page;
+        }
+    },
+    blocks: {
+        /**
+         * Format each exercise.  This is run when HonKit parses the book.
+         */
+        exercise: {
+            process: (block) => {
+                const { label } = block.kwargs;
+                const prefix = !!label ? `<p id="${label}">` : "<p>";
+                const suffix = `<strong>Exercise.</strong>${block.body}</p>`;
+                return `${prefix}${suffix}`;
+            }
         }
     }
 };
